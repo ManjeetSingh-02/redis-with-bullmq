27bf5be927b538f471a000c1e288277929c00a2a
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-03-26 20:59:13 +0530

    add Redis client setup

---
+++ src/core/redis/client.ts
@@ -0,0 +1,11 @@
+// internal-imports
+import { env } from '../config/env.js';
+
+// external-imports
+import { Redis } from 'ioredis';
+
+// create a new redis client
+export const redisClient = new Redis({
+  host: env.REDIS_HOST,
+  port: env.REDIS_PORT,
+});
