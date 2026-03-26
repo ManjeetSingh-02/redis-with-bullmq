ef3ded65fdb63fed22ecb757980e64c103fb8af7
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-03-26 22:35:57 +0530

    add: enhance video processing worker to log details and add email notification

--- src/core/queues/process-video/worker.ts
+++ src/core/queues/process-video/worker.ts
@@ -1,5 +1,6 @@
 // internal-imports
 import { redisClient } from '../../redis/client.js';
+import { emailQueue } from '../email/queue.js';
 import { APP_CONFIG } from '../../config/constants.js';
 
 // external-imports
@@ -8,7 +9,15 @@ import { Worker } from 'bullmq';
 // create a new worker for processing videos
 export const processVideoWorker = new Worker(
   APP_CONFIG.QUEUE_MAP.PROCESS_VIDEO_QUEUE,
-  async job => console.log(`Processing Video(${job.id}): ${job.data.videoURL}`),
+  async job => {
+    // process the video
+    console.log(`Processing Video(${job.id}): URL(${job.data.videoURL})`);
+
+    // add a new job to email queue
+    await emailQueue.add(`email-${job.data.videoURL}`, {
+      message: `Video(${job.data.videoURL}) has been processed successfully`,
+    });
+  },
   {
     connection: redisClient,
     concurrency: 2,
