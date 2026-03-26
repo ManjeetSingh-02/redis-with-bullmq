3e16087ce9c5d3e332e683f07ff2fa095356e8e7
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-03-26 21:16:51 +0530

    feat: add route to enqueue video URLs with validation

--- src/app/express/application.ts
+++ src/app/express/application.ts
@@ -1,5 +1,9 @@
+// internal-imports
+import { processVideoQueue, validateZodSchema } from '@/core/index.js';
+
 // external-imports
 import express from 'express';
+import z from 'zod';
 
 // type-imports
 import type { Application } from 'express';
@@ -11,6 +15,35 @@ export default function createApp(): Application {
 
   // attach middlewares
   application.use(express.json()).use(express.urlencoded({ extended: true }));
+
+  // route to add videoURL to the queue
+  application.post(
+    '/add-video',
+    validateZodSchema(
+      z.object({
+        body: z.object({
+          videoURL: z.url({ error: 'Video URL must be a valid URL' }),
+        }),
+      })
+    ),
+    async (request, response) => {
+      // add video URL to the queue
+      const job = await processVideoQueue.add(`video-${request.body.videoURL}`, {
+        url: request.body.videoURL,
+      });
+
+      // send response
+      return response.status(200).json({
+        success: true,
+        message: 'Video URL added to the queue',
+        data: {
+          jobID: job.id,
+          status: 'enqueued',
+          videoURL: request.body.videoURL,
+        },
+      });
+    }
+  );
 
   // return the application
   return application;
